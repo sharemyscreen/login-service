a5f86bf1ef3acdf0f68bd3141885a5b8ca31c158
Maxime Guedj <max.guedj13@gmail.com>
2016-06-29 00:40:58 +0200

    Add header to all request

--- index.js
+++ index.js
@@ -22,10 +22,9 @@ function getApp () {
 
   basicAuth.init();
 
-  loginApp.all('/', function (req, res, next) {
+  loginApp.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     next();
   });
 
