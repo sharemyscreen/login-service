59e13f2daad1e19a64987e71177f559ec544c208
Maxime Guedj <max.guedj13@gmail.com>
2016-06-29 00:21:46 +0200

    Add header to all request

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ function getApp () {
 
   basicAuth.init();
 
+  loginApp.all('/', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+  });
+
   // Register all routes
   userRoute.registerRoute(loginRouter);
   oauth2Route.registerRoute(loginRouter);
